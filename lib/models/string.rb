834e0c9b9b16dfab1eff2f9bba5e6ca13deefe9d
Lee Patterson <Lee.Patterson@dvla.gov.uk>
2025-02-12 17:41:18 +0000

    adding comment to string.rb

--- lib/models/string.rb
+++ lib/models/string.rb
@@ -1,9 +1,20 @@
 require 'capitalize_names'
 
 class String
+
+  # Monkey patching the Sting class to add methods
+  # @return [String]
+
+  # Use CaptializeNames gem to correctly format first_names
+  # @param [self]
+  # @return [String]
   def capitalize_first_names
     CapitalizeNames.capitalize(self)
   end
+
+  # Correctly formats surname to capitalize first and all letters following an apostrophe
+  # @param record [self]
+  # @return [String]
 
   def capitalize_surname
     if self.chars.include?("'")
